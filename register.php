5e87a6c7cb35c518260e4b595d6dc0486e684dbf
DThePro <104270556+DThePro@users.noreply.github.com>
2024-02-16 01:01:04 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,22 +1,28 @@
-$con = mysqli_connect('sql6.freesqldatabase.com', 'root', 'root', 'sql6684383');
+<?php
 
-if (mysqli_connect_error())
-{
-    echo "ERR1: Connection failed.";
-    exit();
-}
+    $con = mysqli_connect('sql6.freesqldatabase.com', 'root', 'root', 'sql6684383');
+    
+    if (mysqli_connect_error())
+    {
+        echo "ERR1: Connection failed.";
+        exit();
+    }
+    
+    $username = $_POST["name"];
+    $password = $_POST["password"];
+    
+    $namecheckquery = "SELECT username from Users WHERE username = '" . $username . "';";
+    $namecheck = mysqli_query($con, $namecheckquery) or die("ERR2: Name check failed.");
+    
+    if (mysqli_num_rows($namecheck) > 0)
+    {
+        echo "ERR3: Name already exists.";
+        exit();
+    }
+    
+    $insertuserquery = "INSERT INTO Users (username, password) VALUES ('" . $username . "', '" . $password . "');";
+    mysqli_query($con, $insertuserquery) or die("ERR4: Registration failed.");
+    
+    echo "0";
 
-$username = $_POST["name"];
-$password = $_POST["password"];
-
-$namecheckquery = "SELECT username from Users WHERE username = '" . $username . "';";
-$namecheck = mysqli_query($con, $namecheckquery) or die("ERR2: Name check failed.");
-
-if (mysqli_num_rows($namecheck) > 0)
-{
-    echo "ERR3: Name already exists.";
-    exit();
-}
-
-$insertuserquery = "INSERT INTO Users (username, password) VALUES ('" . $username . "', '" . $password . "');";
-mysqli_query($con, $insertuserquery) or die("ERR4: Registration failed.");
+?>
